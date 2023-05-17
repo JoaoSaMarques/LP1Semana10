ec4532b3c15ea1a9b081bb5cd41baf325be13673
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-17 16:12:30 +0100

    Added 5 strings for each element

--- StringCollections/Program.cs
+++ StringCollections/Program.cs
@@ -15,6 +15,32 @@ namespace StringCollections
             Queue<string> queue = new Queue<string>();
             //HashSet T
             HashSet<string> hashSet = new HashSet<string>();
+
+            //Insert five strings into each collection
+            list.AddRange(new string[] { "A", "B", "C", "B", "E" });
+
+            //Stacks following list pattern
+            stack.Push("A");
+            stack.Push("B");
+            stack.Push("C");
+            stack.Push("B");
+            stack.Push("D");
+
+            //Queues following list pattern
+            queue.Enqueue("A");
+            queue.Enqueue("B");
+            queue.Enqueue("C");
+            queue.Enqueue("B");
+            queue.Enqueue("D");
+
+            //HashSets following list pattern
+            hashSet.Add("A");
+            hashSet.Add("B");
+            hashSet.Add("C");
+            hashSet.Add("B");
+            hashSet.Add("D");
         }
+
+        
     }
 }
