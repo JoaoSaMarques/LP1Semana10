1a0d9c30d0e7d0b478b2769ba06cb48e7babb219
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-18 17:43:50 +0100

    Made copy

---
+++ SamePlayer/SamePlayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SamePlayer
+{
+    //Class
+    public enum PlayerType { Tank, Fighter, Slayer, Mage, Controller, Marksmen }
+
+    //Player
+    public class Player
+    {
+        //PlayerType
+        public PlayerType Type { get; set; }
+        public string Name { get; set; }
+
+        
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Player otherPlayer = (Player)obj;
+            return Type == otherPlayer.Type && Name == otherPlayer.Name;
+        }
+
+        //HashCode
+        public override int GetHashCode()
+        {
+            return Type.GetHashCode() ^ Name.GetHashCode();
+        }
+    }
+
+}
